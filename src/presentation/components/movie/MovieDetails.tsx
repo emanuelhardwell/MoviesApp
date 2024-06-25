ba23181012cdf084d982f6f100adf0e59e9b1bca
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-24 22:06:47 -0600

    feat: Add section casts credits

--- src/presentation/components/movie/MovieDetails.tsx
+++ src/presentation/components/movie/MovieDetails.tsx
@@ -2,12 +2,16 @@ import {Text, View} from 'react-native';
 import {MovieFullEntity} from '../../../core/entities/movie-full.entity';
 import {FC} from 'react';
 import {Currency} from '../../../config/helpers/currency';
+import {FlatList} from 'react-native-gesture-handler';
+import {castCreditsEntity} from '../../../core/entities/cast-credits.entity';
+import {CastCredits} from '../cast/CastCredits';
 
 interface MovieDetailsProps {
   movie: MovieFullEntity;
+  casts: castCreditsEntity[];
 }
 
-export const MovieDetails: FC<MovieDetailsProps> = ({movie}) => {
+export const MovieDetails: FC<MovieDetailsProps> = ({movie, casts}) => {
   return (
     <>
       <View style={{marginHorizontal: 20, marginTop: 10, marginBottom: 20}}>
@@ -24,6 +28,18 @@ export const MovieDetails: FC<MovieDetailsProps> = ({movie}) => {
           Presupuesto
         </Text>
         <Text>{Currency.currencyUSD(movie.budget)}</Text>
+
+        <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
+          Actores
+        </Text>
+
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={casts}
+          keyExtractor={cast => `${cast.id}`}
+          renderItem={cast => <CastCredits cast={cast.item} />}
+        />
       </View>
     </>
   );
